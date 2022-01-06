a3aa9ef653a07f16a230f25fd887f5f577ab7e5b
weixing19 <weixing_1976@163.com>
2022-01-06 14:32:50 +0800

    Fix errors caused by code version merging

--- src/main/java/com/pingcap/util/ThreadPoolUtil.java
+++ src/main/java/com/pingcap/util/ThreadPoolUtil.java
@@ -1,14 +1,17 @@
 package com.pingcap.util;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-
 import java.util.concurrent.*;
 
 /**
  * @author yuyang
  */
 public class ThreadPoolUtil {
-
+	
+	public interface RunnerThreadCallBack {
+		public void runnerThreadCallBack();
+	}
+	
     public static ThreadPoolExecutor startJob(int corePoolSize, int maxPoolSize) {
         return new ThreadPoolExecutor(
                 corePoolSize,
@@ -20,22 +23,4 @@ public class ThreadPoolUtil {
                 new ThreadPoolExecutor.CallerRunsPolicy());
     }
     
-/**
- * @author zhugp
- */   
-    public static void forExecutor(ThreadPoolExecutor executor, int cSize){
-        //预防等待队列过载,未完成数量超过设定值则等待。
-    	if(executor.getActiveCount()+executor.getQueue().size() > cSize) {
-            while (true) {
-				try {
-					Thread.sleep(5000);				
-				} catch (InterruptedException e1) {
-					e1.printStackTrace();
-				}
-            	if(executor.getActiveCount()+executor.getQueue().size() < cSize)
-            		break;
-            }
-        }//end if
-    }
-
 }
